1c3111b93a40de3dfe74613e8845109740511b26
MrTBit <timjbart97@gmail.com>
2018-04-15 19:07:55 -0400

    V3 Final

--- retirement-app/routes/retirement.js
+++ retirement-app/routes/retirement.js
@@ -12,20 +12,24 @@ router.post('/', function(req, res, next){
     var age_retirement = parseInt(req.body.age_retirement);
     var age_death = parseInt(req.body.age_death);
     var savings = parseFloat(req.body.savings);
-    var ror = parseFloat(req.body.ror);
+    var ror = parseFloat(req.body.ror)*.01;
     var ror_option = req.body.ror_option;
     var income = parseFloat(req.body.income);
-    var income_rise = parseFloat(req.body.income_rise);
-    var income_to_savings_percent = parseFloat(income_to_savings_percent);
-    var ror_post = parseFloat(req.body.ror_post);
+    var income_rise = parseFloat(req.body.income_rise)*.01;
+    var income_to_savings_percent = parseFloat(req.body.income_to_savings_percent)*.01;
+    var ror_post = parseFloat(req.body.ror_post)*.01;
     var retirement_income = parseFloat(req.body.retirement_income);
     var retirement_expenses = parseFloat(req.body.retirement_expenses);
-    var inflation_rate = parseFloat(req.body.inflation_rate);
+    var inflation_rate = parseFloat(req.body.inflation_rate)*.01;
+    var yearCurrent = new Date().getFullYear();
 
     function FV(rate, nper, pmt, pv, type){
-      var pow = Math.pow(1 + rate, nper), fv;
+     
       pmt = pmt*-1; //needs to be negative
       pv = pv*-1; // also needs to be negative
+
+      var pow = Math.pow(1 + rate, nper), fv;
+      
 
       pv = pv || 0;
       type = type || 0;
@@ -40,8 +44,10 @@ router.post('/', function(req, res, next){
 
 
 
-    var magicNumber = FV(ror, age_retirement-age, income*income_to_savings_percent,savings);
-    console.log(magicNumber);
+    var fv = FV(ror, age_retirement-age, (income*income_to_savings_percent),savings);
+
+
+    res.render('retirementPost', {title: 'Retirement App Results', year: yearCurrent, yearStart: yearCurrent+(age_retirement-age), yearEnd: yearCurrent+(age_death-age), savings: '$' + savings.toFixed(2), fv: '$' + fv.toFixed(2)});
 });
 
 module.exports = router;
